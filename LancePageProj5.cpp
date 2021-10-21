c62c11c6434f283d9de79fbc6233ee4441f1f78e
Lance Page <6ftdotexe@users.noreply.github.com>
2021-10-21 12:30:14 -0500

    Finished and tested

--- LancePageProj5.cpp
+++ LancePageProj5.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void testMenu(){
+// pre-condition: Program started or 7 not selected
 // post-condition: the test menu is displayed for choose
     using namespace std;
     cout << "****************************** \n";
@@ -15,21 +16,34 @@ void testMenu(){
 }
 
 bool isLeapYear(int year){
+//pre-condition: user provides a year
+//post-condition: finds if the year provided is a leap year or not
     return (((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0)));
 }
 
 int getCenturyValue(int year){
+//pre-condition: user provides a year
+//post-condition: finds what century the year is apart of
     int century = year/100;
     int remainder = (century % 4);
     return ((3 - remainder) * 2);
 }
 
 int getYearValue(int year){
-    return (getCenturyValue(year) + (getCenturyValue(year)/4));
+//pre-condition: user provides a year
+//post-condition: finds how many years since the beginning of the century the year is apart of
+    int year1, year2, total;
+    year1 = year / 100;
+    year2 = year - (year1 * 100);
+    total = year2 / 4;
+    return (total + year2);
 }
+
 // fix
 
 int getMonthValue(int month, int year){
+//pre-condition: user provides a month and year in MM YYYY format
+//post-condition: finds what month the the user is providing
     if(month == 1){
         return (isLeapYear(year)) ? 6 : 0;
     }
@@ -72,8 +86,11 @@ int getMonthValue(int month, int year){
 }
 
 int dayOfWeek(int month, int day, int year){
-    day += getMonthValue(month, year) + getYearValue(year) + getCenturyValue(year);
-    return day;
+//pre-condition: user provides day, month, and year in DD MM YYYY format
+//post-condition: finds what day of the month it is (IE M,T,W,TH,F,S,SN)
+    int total1=0;
+    total1 = (day + getMonthValue(month,year) + getYearValue(year) + getCenturyValue(year)) % 7;
+    return total1;
 }
 // test again after getYearValue fixed
 
@@ -81,30 +98,31 @@ std::string dayOfWeek(int day){
 // pre-condition: day has integer value 0, 1, 2, 3, 4, 5, or 6
 // post-condition: the name of the day of week is returned as a std::. If day
 // has value 0, then SUNDAY is returned; 1, then MONDAY is returned; and so on.
-    int x = day % 7;
-    std::string weekday;
-    if(x==0){
-        weekday = "Sunday";
+    std::string total;
+    switch (day){
+        case 0:
+            total = " Sunday ";
+            break;
+        case 1:
+            total = " Monday ";
+            break;
+        case 2:
+            total = " Tuesday ";
+            break;
+        case 3:
+            total = " Wednesday ";
+            break;
+        case 4:
+            total = " Thursday ";
+            break;
+        case 5:
+            total = " Friday ";
+            break;
+        case 6:
+            total = " Saturday ";
+            break;
     }
-    else if(x==1){
-        weekday = "Monday";
-    }
-    else if(x==1){
-        weekday = "Tuesday";
-    }
-    else if(x==1){
-        weekday = "Wednesday";
-    }
-    else if(x==1){
-        weekday = "Thursday";
-    }
-    else if(x==1){
-        weekday = "Friday";
-    }
-    else if(x==1){
-        weekday = "Saturday";
-    }
-    return weekday;
+    return total;
 }
 // fix
 
